181006740945f21f22c907c3435ffac305606282
boo <booblegum42@gmail.com>
2022-11-22 20:02:55 +0700

    add static and name-urls

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home),
-    path('item/<int:id>', views.page_item),
-    path('items', views.items_list),
-]
+    path('', views.home, name="home"),
+    path('item/<int:id>', views.page_item, name="item-detail"),
+    path('items/list', views.items_list, name="items-list"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
